40e914d696d0e248c14d885809286087061360ec
leg <luffio1207@gmail.com>
2026-04-11 18:03:24 +0500

    Increase emoji icon size 32->44

--- lib/widgets/input_bar.dart
+++ lib/widgets/input_bar.dart
@@ -90,8 +90,8 @@ class _InputBarState extends State<InputBar> {
                 onTap: () {},
                 child: SvgPicture.asset(
                   'assets/icons/smile.svg',
-                  width: 32,
-                  height: 32,
+                  width: 44,
+                  height: 44,
                 ),
               ),
 
